4098b0b91df620cf251f31afdd6fa969f137c111
abdikasa <abdikasa@sheridancollege.ca>
2020-08-28 20:02:41 -0400

    Completed addNote functionality

--- src/App.js
+++ src/App.js
@@ -12,14 +12,24 @@ const App = (props) => {
   const addNote = (event) => {
     event.preventDefault();
     let allNotes = [...notes];
-    allNotes.push({ id: notes.length + 1, content: newNote });
+    allNotes.push({
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    });
+
+    //show the new state of all notes
     setNotes(allNotes);
+    //update state of input to blank.
+    setNewNote("");
   };
 
   //   In order to enable editing of the input element, we have to register an event handler that synchronizes the changes made to the input with the component's state:
 
   const handleEvent = (event) => {
     console.log(event.target.value);
+    //needed, otherwise it doesn't show on the browser.
     setNewNote(event.target.value);
   };
 
