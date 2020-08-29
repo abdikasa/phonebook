008198c4923a92aeb0c09afe8165312c8359bf99
abdikasa <abdikasa@sheridancollege.ca>
2020-08-29 16:35:13 -0400

    Starting exercise 2.10

--- src/index.js
+++ src/index.js
@@ -19,4 +19,11 @@ import App from "./App";
 // 2.9*: The Phonebook Step4 ----FINISHED
 // Implement a search field that can be used to filter the list of people by name:
 
+// 2.10: The Phonebook Step5 ----STARTING
+// If you have implemented your application in a single component, refactor it by extracting suitable parts into new components. Maintain the application's state and all event handlers in the App root component.
+
+// It is sufficient to extract three components from the application. Good candidates for separate components are, for example, the search filter, the form for adding new people into the phonebook, a component that renders all people from the phonebook, and a component that renders a single person's details.
+
+// The application's root component could look similar to this after the refactoring. The refactored root component below only renders titles and lets the extracted components take care of the rest.
+
 ReactDOM.render(<App />, document.getElementById("root"));
