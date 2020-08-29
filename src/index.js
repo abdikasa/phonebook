e7634a45df2d7897a8198adfe23a78aa9166b7fd
abdikasa <abdikasa@sheridancollege.ca>
2020-08-28 20:52:42 -0400

    Startin exercise 2.6 w/ exercise instructions

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// 2.6: The Phonebook Step1
+// Let's create a simple phonebook. In this part we will only be adding names to the phonebook.
+// Let us start with implementing the addition of a person to phonebook.
+// The newName state is meant for controlling the form input element.
+
 ReactDOM.render(<App />, document.getElementById("root"));
