7f2b2e31f61f46e1e0d493e316b3ac49106f5bf6
abdikasa <abdikasa@sheridancollege.ca>
2020-08-28 20:51:18 -0400

    Starting exercise 2.6

--- src/App.js
+++ src/App.js
@@ -1,59 +1,22 @@
 import React, { useState } from "react";
-import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState("a new note");
-  const [filtView, setFiltView] = useState(true);
-
-  const filterNotes = filtView ? notes : notes.filter((note) => note.important);
-
-  const addNote = (event) => {
-    event.preventDefault();
-    let allNotes = [...notes];
-    const myNewNote = {
-      id: notes.length + 1,
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-    };
-    if (myNewNote.content.trim() == "") {
-      return;
-    }
-    allNotes.push(myNewNote);
-    //show the new state of all notes
-    setNotes(allNotes);
-    //update state of input to blank.
-    setNewNote("");
-  };
-
-  //   In order to enable editing of the input element, we have to register an event handler that synchronizes the changes made to the input with the component's state:
-
-  const handleEvent = (event) => {
-    console.log(event.target.value);
-    //needed, otherwise it doesn't show on the browser.
-    setNewNote(event.target.value);
-  };
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
   return (
     <div>
-      <h1>Notes</h1>
-      <button
-        onClick={() => {
-          setFiltView(!filtView);
-        }}
-      >
-        show {!filtView ? " all" : "important"};
-      </button>
-      <ul>
-        {filterNotes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleEvent}></input>
-        <button type="submit">Add</button>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
       </form>
+      <h2>Numbers</h2>
+      ...
     </div>
   );
 };
