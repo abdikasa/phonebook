eb933a0d0deee2d686ebb1affea2a77e918f49ef
abdikasa <abdikasa@sheridancollege.ca>
2020-08-30 21:11:21 -0400

    Starting exercise 2.11: Retreiving data from json using axios and useEffect hook

--- src/index.js
+++ src/index.js
@@ -3,27 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-// 2.6: The Phonebook Step1 ----FINISHED
-// Let's create a simple phonebook. In this part we will only be adding names to the phonebook.
-// Let us start with implementing the addition of a person to phonebook.
-// The newName state is meant for controlling the form input element.
+//I deleted teh instructions for 2.1 to 2.10.
+//You can find it in older commits of the app.
 
-// 2.7: The Phonebook Step2 ----FINISHED
-// Prevent the user from being able to add names that already exist in the phonebook. JavaScript arrays have numerous suitable methods for accomplishing this task.
-
-// Issue a warning with the alert command when such an action is attempted:
-
-// 2.8: The Phonebook Step3 ---- FINISHED
-// Expand your application by allowing users to add phone numbers to the phone book. You will need to add a second input element to the form (along with its own event handler):
-
-// 2.9*: The Phonebook Step4 ----FINISHED
-// Implement a search field that can be used to filter the list of people by name:
-
-// 2.10: The Phonebook Step5 ---- FINISHED
-// If you have implemented your application in a single component, refactor it by extracting suitable parts into new components. Maintain the application's state and all event handlers in the App root component.
-
-// It is sufficient to extract three components from the application. Good candidates for separate components are, for example, the search filter, the form for adding new people into the phonebook, a component that renders all people from the phonebook, and a component that renders a single person's details.
-
-// The application's root component could look similar to this after the refactoring. The refactored root component below only renders titles and lets the extracted components take care of the rest.
+//2.11: The Phonebook Step6 -----Starting
+//We continue with developing the phonebook. Store the initial state of the application in the file db.json, which should be placed in the root of the project.
+//Modify the application such that the initial state of the data is fetched from the server using the axios-library. Complete the fetching with an Effect hook.
 
 ReactDOM.render(<App />, document.getElementById("root"));
